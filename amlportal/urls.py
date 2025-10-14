20a929c7397149ab79ccb6ea0731b8d5a385617c
bkh1423 <bkh14231423@gmail.com>
2025-10-14 22:33:49 +0300

    تعديل ملف ال urls العام

--- amlportal/urls.py
+++ amlportal/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include  
+from django.urls import path, include
+from django.shortcuts import render
+
+def home_view(request):
+    return render(request, 'home.html')
 
 urlpatterns = [
-   
     path('admin/', admin.site.urls),
 
-    
-    path('accounts/', include('accounts.urls')),      
-    path('assessment/', include('assessment.urls')),  
-    path('results/', include('results.urls')),        
+    path('', home_view, name='home'),
+
+    path('accounts/', include('accounts.urls')),
+    path('assessment/', include('assessment.urls')),
+    path('results/', include('results.urls')),
 ]
